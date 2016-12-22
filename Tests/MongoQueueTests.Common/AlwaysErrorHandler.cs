6dadeb91302835f7c3e6c6bdd42602f3c1311805
Vadim Zhogalsky <vadim.zhogalsky@solomoto.com>
2016-12-22 17:11:00 +0300

    missing files

---
+++ Tests/MongoQueueTests.Common/AlwaysErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoQueue.Core;
+using MongoQueue.Core.AgentAbstractions;
+
+namespace MongoQueueTests.Common
+{
+    public class AlwaysErrorHandler : MessageHandlerBase<TestMessage>
+    {
+        public AlwaysErrorHandler(IMessageStatusManager messageStatusManager) : base(messageStatusManager)
+        {
+        }
+
+        public override async Task Handle(TestMessage message, bool resend, CancellationToken cancellationToken)
+        {
+            ResultHolder.Add(message.Id + "resend" + Guid.NewGuid(), message.Name);
+            throw new InvalidOperationException();
+        }
+    }
+}
